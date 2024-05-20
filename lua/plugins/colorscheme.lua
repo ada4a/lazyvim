daad2f4807661d50e00ba5020361895e37198825
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-20 18:55:01 +0200

    (lazy): move colorscheme config to a separate file

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "savq/melange-nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "rose-pine/neovim" },
+  { "https://gitlab.com/protesilaos/tempus-themes-vim.git" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "melange" },
+  },
+}
