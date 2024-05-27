82da2e014516b5bd7f646103c17dd7a9ab8bdbe6
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-05-27 17:56:58 +0200

    (telescope): incorporate project.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,4 +27,25 @@ return {
       desc = "Colorscheme with Preview",
     },
   },
+  -- add project dirs for project.nvim
+  dependencies = {
+    "nvim-telescope/telescope-project.nvim",
+    keys = {
+      -- need to use this as soon as I open the app
+      -- because otherwise the plugin loading order gets messed up or something
+      -- https://github.com/LazyVim/LazyVim/issues/283#issuecomment-1433390326
+      { "<C-p>", "<cmd>lua require('telescope').extensions.project.project{}<cr>", desc = "Telescope Project" },
+      config = function()
+        require("telescope").load_extension("project")
+      end,
+    },
+  },
+  opts = {
+    extensions = {
+      project = { base_dirs = {
+        { path = "~/Code", max_depth = 4 },
+        { path = "~/.dotfiles" },
+      } },
+    },
+  },
 }
