3a995986cd24255fa3aa6163fa14c40a3a729853
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-17 11:59:55 +0200

    fix: disable `mason-nvim-dap` (for the same reason as `mason` itself)

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   -- Manage packages using Nix
   { "williamboman/mason.nvim", enabled = false },
+  -- Installs and sets up DAPs via mason, so turn it off as well
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 }
