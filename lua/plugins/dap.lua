27ca2bfc5de11498705ae5b871825be36713254d
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-06-17 09:26:54 +0200

    style(dap): rm `stylua: ignore` since the line is short enough already

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,11 +1,10 @@
 local dap = require("dap")
 return {
   "mfussenegger/nvim-dap",
-  -- stylua: ignore
   keys = {
-    {"<F5>",  dap.continue,  desc = "Continue"},
-    {"<F10>", dap.step_over, desc = "Step Over"},
-    {"<F11>", dap.step_into, desc = "Step Into"},
-    {"<F12>", dap.step_out,  desc = "Step Out"}
+    { "<F5>", dap.continue, desc = "Continue" },
+    { "<F10>", dap.step_over, desc = "Step Over" },
+    { "<F11>", dap.step_into, desc = "Step Into" },
+    { "<F12>", dap.step_out, desc = "Step Out" },
   },
 }
