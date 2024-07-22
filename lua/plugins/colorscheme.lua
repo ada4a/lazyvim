16c0c53d74ebffdef13364191d78ee73ad656362
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-22 20:08:20 +0200

    feat(colorschemes): rm unused ones

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,4 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  { "savq/melange-nvim" },
   { "EdenEast/nightfox.nvim" },
   { "rose-pine/neovim" },
-  { "https://gitlab.com/protesilaos/tempus-themes-vim.git" },
 }
