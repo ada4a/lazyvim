d9b7072d73015c3790a48c8c672fd1b6764f0336
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-23 20:24:22 +0200

    feat(lsp/nixd): set-up autocompletion

--- lua/plugins/nix.lua
+++ lua/plugins/nix.lua
@@ -1,9 +1,24 @@
 -- Nix is foing to get its own Extra soon, so here I've collected
 -- nix-specific settings to delete them all at once
+
+-- TODO change these to use flake.outPath, systemSettings.hostname, and userSettings.username, respectively
+local flake = '(builtins.getFlake "$HOME/.dotfiles")'
+local nixos = "magicbook"
+local user = "ugura"
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = { servers = { nixd = {} } },
+    opts = {
+      servers = {
+        nixd = {
+          nixpkgs = { expr = string.format("import %s.inputs.nixpkgs {}", flake) },
+          options = {
+            nixos = { expr = string.format("%s.nixosConfigurations.%s.options", flake, nixos) },
+            home_manager = { expr = string.format("%s.homeConfigurations.%s.options", flake, user) },
+          },
+        },
+      },
+    },
   },
   {
     "stevearc/conform.nvim",
