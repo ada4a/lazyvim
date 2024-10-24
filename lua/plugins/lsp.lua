f20c67e6a929bf07177ce43af0c9c354ace1c228
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-24 12:17:42 +0200

    feat(nvim-lspconfig): rm configuration that is default (in LazyVim)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    -- turn this off since none of the languages I use support it
-    codelens = { enabled = false },
     servers = {
       lua_ls = {},
     },
