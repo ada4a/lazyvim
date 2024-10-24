74428a8a1964eb1ed9211d7dc5e76fb346411ddb
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-10-24 11:25:41 +0200

    feat(keymaps): add my own mildly cursed light/dark theme toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,17 @@ LazyVim.toggle.map(
   "<leader>uC",
   LazyVim.toggle("conceallevel", { values = { 0, vim.o.conceallevel > 0 and vim.o.conceallevel or 2 } })
 )
+
+vim.keymap.set("n", "<leader>ut", function()
+  local themes = { light = "rose-pine-dawn", dark = "terafox" }
+
+  if vim.o.background == "light" then
+    vim.cmd.colorscheme(themes.dark)
+  else
+    vim.cmd.colorscheme(themes.light)
+    -- NOTE: setting "rose-pine-dawn" by itself doesn't change `background`
+    -- NOTE: need to set the colorscheme first,
+    -- otherwise brackets will retain their color from terafox
+    vim.o.background = "light"
+  end
+end, { desc = "Toggle dark/light theme" })
