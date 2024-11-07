0db2367b1bf477193f26910eaca66aef6a570efe
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-11-07 14:58:10 +0100

    feat(treesitter): disable

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,8 @@ return {
   { "williamboman/mason.nvim", enabled = false },
   -- Installs and sets up DAPs via mason, so turn it off as well
   { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  -- Manage grammars using Nix
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  -- Uses nvim-treesitter, so turn it off as well
+  { "nvim-treesitter-textobjects", enabled = false },
 }
