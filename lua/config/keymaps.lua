e58623a20cb39ed79aa804852dc63d2845f3720b
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-11-10 17:19:32 +0100

    refactor(keymaps): toggle background using the new Snacks API

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,16 +5,26 @@ Snacks.toggle
   .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
   :map("<leader>uC")
 
-vim.keymap.set("n", "<leader>ut", function()
-  local themes = { light = "rose-pine-dawn", dark = "terafox" }
-
-  if vim.o.background == "light" then
-    vim.cmd.colorscheme(themes.dark)
-  else
-    vim.cmd.colorscheme(themes.light)
-    -- NOTE: setting "rose-pine-dawn" by itself doesn't change `background`
-    -- NOTE: need to set the colorscheme first,
-    -- otherwise brackets will retain their color from terafox
-    vim.o.background = "light"
-  end
-end, { desc = "Toggle dark/light theme" })
+-- Modify the default background toggling to also change the theme
+vim.keymap.del("n", "<leader>ub")
+Snacks.toggle
+  .new({
+    name = "Light theme",
+    get = function()
+      return vim.o.background == "light"
+    end,
+    set = function(state)
+      local themes = { light = "rose-pine-dawn", dark = "terafox" }
+      -- XXX: no idea why the `not` is necessary here
+      if not state then
+        vim.cmd.colorscheme(themes.dark)
+      else
+        vim.cmd.colorscheme(themes.light)
+        -- NOTE: setting "rose-pine-dawn" by itself doesn't change `background`
+        -- NOTE: need to set the colorscheme first,
+        -- otherwise brackets will retain their color from terafox
+        vim.o.background = "light"
+      end
+    end,
+  })
+  :map("<leader>ub")
