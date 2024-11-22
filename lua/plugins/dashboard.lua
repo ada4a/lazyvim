f0c835c1080c56b23c1420f1607c096bb54ad689
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-11-22 13:38:01 +0100

    fix(dashboard): re-add the default values (since they get overwritten otherwise)

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -5,7 +5,17 @@ return {
       preset = {
         -- stylua: ignore
         keys = {
+          -- default keys - kept since otherwise my config seems to overwrite the default list?
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "d", desc = "Dotfiles", action = ":lua Snacks.dashboard.pick('files', { cwd = '~/.dotfiles' })", }, -- my addition
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
     },
