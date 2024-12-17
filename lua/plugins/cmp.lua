ba558004fba15a1b7f323bd11db5f6b6f513c6b7
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-12-17 13:05:48 +0100

    fix(cmp): change to blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,10 +1,10 @@
 return {
-  "hrsh7th/nvim-cmp",
-  opts = {
-    -- https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/plugins/coding.lua#L26
-    auto_select = false,
-    completion = {
-      autocomplete = false,
-    },
-  },
+  "folke/blink.cmp",
+  -- opts = {
+  --   -- https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/plugins/coding.lua#L26
+  --   auto_select = false,
+  --   completion = {
+  --     autocomplete = false,
+  --   },
+  -- },
 }
