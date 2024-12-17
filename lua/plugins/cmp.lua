89c4ff5044987bbf9bff9cba5773ebe7e1daf5c5
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-12-17 13:09:30 +0100

    feat(cmp): disable ghost_text
    god it's so annoying

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,10 +1,10 @@
 return {
   "folke/blink.cmp",
-  -- opts = {
-  --   -- https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/plugins/coding.lua#L26
-  --   auto_select = false,
-  --   completion = {
-  --     autocomplete = false,
-  --   },
-  -- },
+  opts = {
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+    },
+  },
 }
