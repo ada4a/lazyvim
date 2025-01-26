d7d176042cdec5ba8ae36486a4c1508d0a1b1afd
Ada Alakbarova <ada.alakbarova@proton.me>
2025-01-26 15:54:43 +0100

    feat(cmp): don't select completions with arrow buttons

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,10 @@
 return {
   "Saghen/blink.cmp",
   opts = {
+    keymap = {
+      ["<Up>"] = {},
+      ["<Down>"] = {},
+    },
     completion = {
       ghost_text = {
         enabled = false,
