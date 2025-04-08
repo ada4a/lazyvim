5b8c429b2f28f3cad7039073ef98639b78c7fd7b
Ada Alakbarova <ada.alakbarova@proton.me>
2025-04-08 19:02:28 +0200

    feat(lang/rust): don't prefer no-std
    I don't even do anything embedded, and most of the time I'm just left
    with two big import sets

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,14 +1,4 @@
 return {
   { "neovim/nvim-lspconfig", opts = { servers = { rust_analyzer = { auto_start = false } } } },
   { "stevearc/conform.nvim", opts = { rust = { "rustfmt" } } },
-  {
-    "mrcjkb/rustaceanvim",
-    opts = {
-      server = {
-        default_settings = { ["rust-analyzer"] = {
-          imports = { preferNoStd = true },
-        } },
-      },
-    },
-  },
 }
