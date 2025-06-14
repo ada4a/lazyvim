4ce6e2c5c27928829591c03e2dcb7b234537cccd
Ada Alakbarova <ada.alakbarova@proton.me>
2025-06-14 18:16:29 +0200

    feat(rust): add shortcuts for "move item up/down"

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,4 +1,13 @@
 return {
   { "neovim/nvim-lspconfig", opts = { servers = { rust_analyzer = { auto_start = false } } } },
   { "stevearc/conform.nvim", opts = { rust = { "rustfmt" } } },
+  {
+    "mrcjkb/rustaceanvim",
+    -- stylua: ignore
+    keys = {
+      {"<leader>cm",  group = "move item"},
+      {"<leader>cmu", function() vim.cmd.RustLsp({ "moveItem", "up" }) end, desc = "Move Item Up"},
+      {"<leader>cmd", function() vim.cmd.RustLsp({ "moveItem", "down" }) end, desc = "Move Item Down"},
+    },
+  },
 }
