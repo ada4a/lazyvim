00ce36f369d48afa5016e32ebdfac9dd0c589188
Ada Alakbarova <ada.alakbarova@proton.me>
2025-11-09 09:41:20 +0100

    chore(mason): asjust to new repo path

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   -- Manage packages using Nix
-  { "williamboman/mason.nvim", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
   -- Installs and sets up DAPs via mason, so turn it off as well
   { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 }
