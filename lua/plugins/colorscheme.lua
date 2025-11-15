1e2a82868046546903398ecc2db5ed8b892d5554
Ada Alakbarova <ada.alakbarova@proton.me>
2025-11-15 15:04:02 +0100

    feat(colorscheme): start with the right dark/light mode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,18 @@
 return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        local path = vim.fs.abspath("~/.config/cosmic/com.system76.CosmicTheme.Mode/v1/is_dark")
+        local is_dark = io.open(path):read()
+        if is_dark == "true" then
+          vim.cmd("colorscheme terafox")
+        else
+          vim.cmd("colorscheme rose-pine-dawn")
+        end
+      end,
+    },
+  },
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "EdenEast/nightfox.nvim" },
